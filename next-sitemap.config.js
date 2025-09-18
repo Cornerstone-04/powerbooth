5cf53883435d8393d2752a2a40457d8c9e4ad4db
Cornerstone <fortunecornerstone@gmail.com>
2025-09-18 15:50:30 +0100

    generate sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: "https://powersafetyboots.com",
+    siteUrl: "https://www.powersafetyboots.com",
     generateRobotsTxt: true, // Generates robots.txt as well
     sitemapSize: 7000,
 };
